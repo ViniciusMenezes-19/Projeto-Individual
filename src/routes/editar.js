1503730864d94786d79674c590f601d4bbbe9403
Vinicius Menezes <114527217+japa0606@users.noreply.github.com>
2026-05-30 23:33:29 -0300

    PROJETO FINALIZADO

--- src/routes/editar.js
+++ src/routes/editar.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var editarController = require("../controllers/editarController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/atualizar", function (req, res) {
     editarController.atualizar(req, res);
 })
